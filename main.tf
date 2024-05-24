71b33490f39d0737e96c6c4f9bc7e9e64562e12b
meenigenikhitha <168175568+meenigenikhitha@users.noreply.github.com>
2024-05-24 15:00:23 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  
+}
+
+resource "aws_instance" "dependency" {
+    ami = "ami-0cc9838aa7ab1dce7"
+  instance_type = "t2.micro"
+  key_name =  "nikki-keypair"
+  tags = {
+    Name = "vasu"
+  }
+}
